697d54f15dfb496e874e8746803bea1cc724c345
ibrahim <ibrdrahim@gmail.com>
2024-03-16 22:56:46 +0700

    Initial Commit

---
+++ Codigo UI Design/Codigo_UI_DesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  Codigo_UI_DesignApp.swift
+//  Codigo UI Design
+//
+//  Created by Ibrahim Baisa on 16/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Codigo_UI_DesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Codigo UI Design/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Codigo UI Design
+//
+//  Created by Ibrahim Baisa on 16/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
